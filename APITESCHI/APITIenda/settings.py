07f8717c6c1b814238abef0c7acd5d2cc6f0eddd
BrianG78 <2019452041@teschi.edu.mx>
2023-09-26 19:29:28 -0600

    restableciendo

--- APITESCHI/APITIenda/settings.py
+++ APITESCHI/APITIenda/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Cremeria',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Cremeria/Cremeria/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-
-class home(APIView):
-    template_name = 'index.html'
-    def get(self,request):
-        return render(request,self.template_name)
